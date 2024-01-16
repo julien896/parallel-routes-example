5d4a3c4c72b8c600dab54d5157dcfc9c73a4094b
Julian <julicogui@gmail.com>
2024-01-16 18:16:08 -0300

    feat: default pages

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,18 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header className='py-10'>
-      Header
+      <div className='container'>
+        <nav>
+          <ul className='flex items-center justify-center gap-10 text-lg font-bold uppercase tracking-wider text-gray-500'>
+            <li>
+              <Link href='/'>Home</Link>
+            </li>
+            <li>
+              <Link href='/settings'>Settings</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
